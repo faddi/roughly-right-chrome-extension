f474f8351eb4aa574f1ff2d3fd521124a9fe519c
Marcus Thelin <marcus.thelin@24hr.se>
2021-09-05 22:13:37 +0200

    Better network listening

--- background.js
+++ background.js
@@ -4,25 +4,22 @@ function sleep(ms = 1000) {
     });
 }
 
-let loaded = false;
-chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && !loaded) {
-        loaded = true;
 
-        chrome.webRequest.onCompleted.addListener(
-            async details => {
-                console.log(details);
-                const { method } = details;
-                await sleep();
-                chrome.tabs.sendMessage(tabId, { summarizeRoughly: true });
-            },
-            {
-                tabId,
-                urls: [
-                    'https://roughlyright.com/employees/*/hours*',
-                    'https://roughlyright.com/employees/*/hours/',
-                ],
-            }
-        );
-    }
-});
+function createNetworkListener() {
+    console.log('Creating listener');
+    chrome.webRequest.onCompleted.addListener(
+        async details => {
+            console.log(details);
+            const { method, tabId } = details;
+            await sleep();
+            chrome.tabs.sendMessage(tabId, { summarizeRoughly: true });
+        },
+        {
+            urls: [
+                'https://roughlyright.com/employees/*/hours*',
+            ],
+        }
+    );
+}
+
+createNetworkListener();
